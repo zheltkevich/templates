cda496620d939f24a4a272fa6ea26dbf48772809
Aleksandr <zheltkevich42@gmail.com>
2021-11-25 22:14:52 +0300

    Update routes

---
+++ src/components/AwesomeComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="hello">
+        <h1>{{ greetings }}</h1>
+        <p>
+        For a guide and recipes on how to configure / customize this project,<br>
+        check out the
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AwesomeComponent',
+    props: {
+        greetings: {
+            type: String,
+            default: 'Hello! I am your awesome component!',
+        },
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  margin: 40px 0 0;
+  text-align: center;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
